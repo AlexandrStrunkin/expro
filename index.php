6c283aa701e62270746dcf6e251ec4c92618fd35
Alexander <faster@webgk.ru>
2016-06-23 10:09:17 +0300

    index update

--- index.php
+++ index.php
@@ -4,5 +4,5 @@ $APPLICATION->SetPageProperty("description", "Мы предлагаем широкий ассортимент 
 $APPLICATION->SetPageProperty("keywords", "офисная мебель");
 $APPLICATION->SetPageProperty("title", "Офисная мебель оптом от производителя, корпусная мебель - фабрика офисной мебели Экспро");
 $APPLICATION->SetTitle("Фабрика офисной мебели Экспро");
-?>   
+?>     
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
